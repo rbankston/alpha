7723f5292249bd069cc6de422f08fb18d997ec19
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-07-01 01:37:21 -0400

    Initial commit

---
+++ calculator.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+# Ruby Diet Calculator for 3 phases of Engineering the Alpha
+
+#first option is what phase of the program you are in
+puts "What phase of Engineering the Alpha are you in: (Please enter 1 2 3 4)"
+phase = gets
+
+puts phase 
+
---
+++ percentage.rb
@@ -0,0 +1,1 @@
+#Percentage measurement portion
