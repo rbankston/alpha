3c108e0c3bc245223af830f3b977b37566773c8f
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-07-03 01:20:18 -0400

    Refactored code

--- calculator.rb
+++ calculator.rb
@@ -17,67 +17,13 @@ pcnt = gets.to_i
 percentage1 = Percentage.new(phase, weight, pcnt)
 
   if phase == 1
-    wo_protein = 0.8 * percentage1.lbm_number.to_f
-    wo_calories = percentage1.maint_calories.to_i - 300
-    nwo_calories = percentage1.maint_calories.to_i - 500
-    nwo_protein = 0.7 * percentage1.lbm_number.to_f
-    nwo_base = nwo_calories.to_i - (nwo_protein.to_i * 4)
-    wo_base = wo_calories.to_i - (wo_protein.to_i * 4) 
-    begin_carb = 30
-    wk3_carb = 75
-    wk4_carb_wo = 100
-    wk4_carb_nwo = 50
-    wo_fat3 = (wo_base.to_i - (wk3_carb.to_i * 4))/9
-    wo_fat4 = (wo_base.to_i - (wk4_carb_wo.to_i * 4))/9
-    nwo_fat4 = (wo_base.to_i - (wk4_carb_nwo.to_i * 4))/9
-    nwo_fat14 = (nwo_base.to_i)/9
-    puts "\nCalories on Workout days are #{wo_calories}. Protein on workout days are #{wo_protein} grams. 
-    Carbs on workout days are #{wk3_carb} grams. Fat on workout days are #{wo_fat3} grams. 
-    
-    Calories on non workout days are #{nwo_calories}. Protein on non workout days are #{nwo_protein} grams. 
-    Carbs on non workout days are #{wk4_carb_nwo} grams. Fat on non workout days are #{nwo_fat14}."
+    percentage1.report1
   elsif phase == 2 
-    wo_calories = percentage1.maint_calories.to_i - 200
-    nwo_calories = percentage1.maint_calories.to_i - 600
-    wo_protein = percentage1.lbm_number.to_f
-    nwo_protein = 0.8 * percentage1.lbm_number.to_f
-    wo_carbs = 0.75 * percentage1.lbm_number.to_f
-    nwo_carbs = 0.3 * percentage1.lbm_number.to_f
-    wo_fat = (wo_calories.to_f - (wo_protein.to_f * 4) - (wo_carbs.to_f * 4))/9
-    nwo_fat = (nwo_calories.to_f - (nwo_protein.to_f * 4) - (nwo_carbs.to_f * 4))/9
-    puts "\nCalories on Workout days are #{wo_calories}. Protein on workout days are #{wo_protein} grams. 
-    Carbs on workout days are #{wo_carbs} grams. Fat on workout days are #{wo_fat} grams. 
-    
-    Calories on non workout days are #{nwo_calories}. Protein on non workout days are #{nwo_protein} grams. 
-    Carbs on non workout days are #{nwo_carbs} grams. Fat on non workout days are #{nwo_fat}."
+    percentage1.report2
   elsif phase == 3
-    wo_calories = percentage1.maint_calories.to_i + 400
-    nwo_calories = percentage1.maint_calories.to_i - 200
-    wo_protein = 1.5 * percentage1.lbm_number.to_f
-    nwo_protein = 1.25 * percentage1.lbm_number.to_f
-    wo_carbs = 1 * percentage1.lbm_number.to_f
-    nwo_carbs = 0.5 * percentage1.lbm_number.to_f
-    wo_fat = (wo_calories - (wo_protein.to_f * 4) - (wo_carbs.to_f * 4))/9
-    nwo_fat = (nwo_calories - (nwo_protein.to_f * 4) - (nwo_carbs.to_f * 4))/9
-    puts "\nCalories on Workout days are #{wo_calories}. Protein on workout days are #{wo_protein} grams. 
-    Carbs on workout days are #{wo_carbs} grams. Fat on workout days are #{wo_fat} grams. 
-    
-    Calories on non workout days are #{nwo_calories}. Protein on non workout days are #{nwo_protein} grams. 
-    Carbs on non workout days are #{nwo_carbs} grams. Fat on non workout days are #{nwo_fat}."
+   percentage1.report3
   elsif phase == 4
-    wo_calories = percentage1.maint_calories.to_i + 300
-    nwo_calories = percentage1.maint_calories.to_i - 400
-    wo_protein = 1.5 * percentage1.lbm_number.to_f
-    nwo_protein = 1 * percentage1.lbm_number.to_f
-    wo_carbs = 1 * percentage1.lbm_number.to_f
-    nwo_carbs = 0.25 * percentage1.lbm_number.to_f
-    wo_fat = (wo_calories - (wo_protein.to_f * 4) - (wo_carbs.to_f * 4))/9
-    nwo_fat = (nwo_calories - (nwo_protein.to_f * 4) - (nwo_carbs.to_f * 4))/9
-    puts "\nCalories on Workout days are #{wo_calories}. Protein on workout days are #{wo_protein} grams. 
-    Carbs on workout days are #{wo_carbs} grams. Fat on workout days are #{wo_fat} grams. 
-    
-    Calories on non workout days are #{nwo_calories}. Protein on non workout days are #{nwo_protein} grams. 
-    Carbs on non workout days are #{nwo_carbs} grams. Fat on non workout days are #{nwo_fat}."
+    percentage1.report4
   end
     
 
--- percentage.rb
+++ percentage.rb
@@ -41,6 +41,63 @@ class Percentage
     @maint_calories = lbm_rate.to_f * lbm_number.to_f
   end
   
+  def printout
+    puts "\nCalories on Workout days are #{@wo_calories}. Protein on workout days are #{@wo_protein} grams. 
+    Carbs on workout days are #{@wo_carbs} grams. Fat on workout days are #{@wo_fat} grams. 
+    
+    Calories on non workout days are #{@nwo_calories}. Protein on non workout days are #{nwo_protein} grams. 
+    Carbs on non workout days are #{@nwo_carbs} grams. Fat on non workout days are #{@nwo_fat}."
+  end
+  
+  def report1
+    @wo_protein = 0.8 * lbm_number.to_f
+    @wo_calories = maint_calories.to_i - 300
+    @nwo_calories = maint_calories.to_i - 500
+    @nwo_protein = 0.7 * lbm_number.to_f 
+    @wo_carbs = 0
+    @nwo_carbs = 0
+    @wo_fat = (@wo_calories.to_f - (@wo_protein.to_f * 4) - (@wo_carbs.to_f * 4))/9
+    @nwo_fat = (@nwo_calories.to_f - (@nwo_protein.to_f * 4) - (@nwo_carbs.to_f * 4))/9
+    printout
+    puts "\tDuring Week 3 you start 30 grams of carbs on workout days."
+    puts "\tDuring Week 4 you get 100 grams of carbs on workout days and 70 on non-workout days"
+  end
+  
+  def report2
+    @wo_calories = maint_calories.to_i - 200
+    @nwo_calories = maint_calories.to_i - 600
+    @wo_protein = lbm_number.to_f
+    @nwo_protein = 0.8 * lbm_number.to_f
+    @wo_carbs = 0.75 * lbm_number.to_f
+    @nwo_carbs = 0.3 * lbm_number.to_f
+    @wo_fat = (@wo_calories.to_f - (@wo_protein.to_f * 4) - (@wo_carbs.to_f * 4))/9
+    @nwo_fat = (@nwo_calories.to_f - (@nwo_protein.to_f * 4) - (@nwo_carbs.to_f * 4))/9
+    printout
+  end
+  
+  def report3
+    @wo_calories = maint_calories.to_i + 400
+    @nwo_calories = maint_calories.to_i - 200
+    @wo_protein = 1.5 * lbm_number.to_f
+    @nwo_protein = 1.25 * lbm_number.to_f
+    @wo_carbs = 1 * lbm_number.to_f
+    @nwo_carbs = 0.5 * lbm_number.to_f
+    @wo_fat = (@wo_calories - (@wo_protein.to_f * 4) - (@wo_carbs.to_f * 4))/9
+    @nwo_fat = (@nwo_calories - (@nwo_protein.to_f * 4) - (@nwo_carbs.to_f * 4))/9
+    printout
+  end
+  
+  def report4
+    @wo_calories = maint_calories.to_i + 300
+    @nwo_calories = maint_calories.to_i - 400
+    @wo_protein = 1.5 * lbm_number.to_f
+    @nwo_protein = 1 * lbm_number.to_f
+    @wo_carbs = 1 * lbm_number.to_f
+    @nwo_carbs = 0.25 * lbm_number.to_f
+    @wo_fat = (@wo_calories - (@wo_protein.to_f * 4) - (@wo_carbs.to_f * 4))/9
+    @nwo_fat = (@nwo_calories - (@nwo_protein.to_f * 4) - (@nwo_carbs.to_f * 4))/9
+    printout
+  end
 
       
  
