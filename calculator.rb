22347eb5356fedf86aaea671b20e8f411e93c485
rbankston <rbankston@engineyard.com>
2014-07-28 23:37:40 -0400

    Changed the calc input a little bit

--- calculator.rb
+++ calculator.rb
@@ -5,13 +5,13 @@ require_relative 'lib/percentage'
 
 
 #first option is what phase of the program you are in
-puts "What phase of Engineering the Alpha are you in: (Please enter 1 2 3 4)"
+print "What phase of Engineering the Alpha are you in: (Please enter 1 2 3 4): "
 phase = gets.to_i
 
-puts "What is your current body weight? (Please only the weight in pounds)"
+print "What is your current body weight? (Please only the weight in pounds): "
 weight = gets.to_i
 
-puts "What is your body fat percentage?(Please only include the number)"
+print  "What is your body fat percentage?(Please only include the number): "
 pcnt = gets.to_i
 
 percentage1 = Percentage.new(phase, weight, pcnt)
